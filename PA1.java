0d38f76fc043794727b9391d58bdcc5ea563fe34
Huntdawg13 <huntermcclure13@gmail.com>
2020-09-20 22:37:05 -0700

    Added comments

--- PA1.java
+++ PA1.java
@@ -1,8 +1,16 @@
+/*
+Name: Hunter McClure
+CptS 233: PA #1
+Date: 9/20/2020
+gitRepo url: https://github.com/Huntdawg13/PA1.git
+
+*/
 import java.util.*; 
 import java.io.*;
   
 public class PA1 
 {     
+    //Main program that adds file into sorted linked list
     public static void main(String args[]) 
     throws FileNotFoundException
     {
@@ -10,6 +18,7 @@ public class PA1
       long endTime = System.nanoTime();
       double duration;
       
+      //Asks for file name
       File file;
       Scanner console = new Scanner(System.in);
 		System.out.print("Enter integer-filled text file here: ");
@@ -17,7 +26,8 @@ public class PA1
       file = new File(FileName);
       
       LinkedList<Integer> ll = new LinkedList<Integer>();
-
+      
+      //Checks to see if file exists and if not then re-asks user
       while(!(file).exists())
       { 
           System.out.println("ERROR: NO FILE FOUND");
@@ -26,6 +36,7 @@ public class PA1
           file = new File(FileName);
       }
       
+      //Adds file into Linked List and sorts
       Scanner input = new Scanner(file);      
       int grabbedNumber;
       ll.add(input.nextInt());
@@ -56,23 +67,27 @@ public class PA1
              else continue;
          }
       }
+      //Prints Linked List and how long it took to add and sort
       endTime = System.nanoTime();
       System.out.println(ll);
       duration = (endTime - startTime)/1000;
-      System.out.println("It took " + duration + " microseconds to sort"); 
+      System.out.println("It took " + duration + " microseconds to insert and sort"); 
       
+      //Gets minimum number and finds how long it takes
       startTime = System.nanoTime(); 
       System.out.println("The minimum value is: " + ll.getFirst());
       endTime = System.nanoTime();
       duration = (endTime - startTime)/1000;
       System.out.println("It took " + duration + " microseconds to find the minimum"); 
       
+      //Gets median number and finds how long it takes
       startTime = System.nanoTime();
       getMed(ll);
       endTime = System.nanoTime();
       duration = (endTime - startTime)/1000;
       System.out.println("It took " + duration + " microseconds to find the median");
              
+      //Gets maximum number and finds how long it takes       
       startTime = System.nanoTime();
       System.out.println("The maximum value is: " + ll.getLast());
       endTime = System.nanoTime();
@@ -80,7 +95,7 @@ public class PA1
       System.out.println("It took " + duration + " microseconds to find the maximum");   
     }
      
-     
+     //Method used to find median number
      public static void getMed(LinkedList<Integer> ll)
      {
          if(ll.size() % 2 == 0)
